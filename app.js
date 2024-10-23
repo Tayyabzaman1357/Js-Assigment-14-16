6b24c926a73a5784c5f5c7485d196dd66cf9a51c
Tayyab Zaman <tayyabzaman807@gmail.com>
2024-10-23 00:26:12 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,25 @@
+ var user=prompt('Enter your name');
+ var name='Tayyab';
+ if (user == name) {
+    document.write(name)
+ } else {
+     document.write("Enter write name?")
+ }
+
+// QNO:2
+
+ var fruits=[''];
+
+// Qno:3;
+
+ var num = [''];
+
+// Qno:4;
+
+ var mixed=["Tayyab",9];
+
+// Qno:5;
+
+ var qualification=['SSC <br>', 'HSC <br>', 'BSC <br>','BS <br>', 'BCOM <br>', 'MS <br>' , 'M.Phil. <br>' , 'PHD <br>'];
+ document.write(qualification);
+
